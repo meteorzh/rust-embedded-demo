5874e832b041831d5d0c2516dc29c149b63f12dc
wzhwayne <519720649@qq.com>
2024-08-04 14:25:51 +0800

    可以运行了

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+#![no_std]
+#![no_main]
+
+// pick a panicking behavior
+use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
+// use panic_abort as _; // requires nightly
+// use panic_itm as _; // logs messages over ITM; requires ITM support
+// use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
+
+use cortex_m_rt::entry;
+use stm32f1xx_hal::{pac, prelude::*};
+
+#[entry]
+fn main() -> ! {
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let dp = pac::Peripherals::take().unwrap();
+
+    let mut flash = dp.FLASH.constrain();
+    let rcc = dp.RCC.constrain();
+
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
+    let mut gpioc = dp.GPIOC.split();
+    let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
+
+    let mut delay = cp.SYST.delay(&clocks);
+
+    loop {
+        // your code goes here
+        led.toggle();
+
+        delay.delay_ms(5000_u16);
+    }
+}
